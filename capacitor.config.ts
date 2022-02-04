e753d213ba8319efd252c0549aa1faaf228547b3
umarmemon1910 <umarmemon1910@gmail.com>
2022-02-04 15:02:26 +0530

    first and final commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.umar.plugin',
+  appName: 'umarPlufin',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-
+import { Plugins } from '@capacitor/core';
+import 'contacts-plugin';
+const { ContactsPlugin } = Plugins;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  contacts = [];
 
-  constructor() {}
+  constructor() {
+  }
 
+  async loadContacts() {
+    this.contacts = (await ContactsPlugin.getContacts('somefilter')).results;
+    console.log('my contacts: ', this.contacts);
+  }
 }
